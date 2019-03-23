5d16035ba2611d717840681382d07cb3740d3876
Filipe Lima <lipeflorentino@yahoo.com>
2019-03-23 13:11:34 +0000

    ajustando sidebar

--- src/component/sidenav/javascript/sidenav-script.js
+++ src/component/sidenav/javascript/sidenav-script.js
@@ -30,30 +30,28 @@
                 }
             $('.vNav ul li a').removeClass('active');
             $('.vNav ul li a:eq('+index+')').addClass('active');
-            if(position < 90){
-                $('.vNav').hide();
-            }
-            else{
+            
+            if(position > 560){
                 $('.vNav').show();
             }
-            if(position < 700){
-                $('.vNav ul li').addClass('vNav-cor1');
+            else{
+                $('.vNav').hide();
             }
-            else if(position <1400 && position > 700){
-                $('.vNav ul li').removeClass('vNav-cor1');
-                $('.vNav ul li').addClass('vNav-cor2');
+            if(position <1200 && position > 560){
+                $('.vNav').removeClass('vNav-cor2');
+                $('.vNav').addClass('vNav-cor1');
             }
-            else if(position <2100 && position > 1400){
-                $('.vNav ul li').removeClass('vNav-cor2');
-                $('.vNav ul li').addClass('vNav-cor1');
+            else if(position <1800 && position > 1200){
+                $('.vNav').removeClass('vNav-cor1');
+                $('.vNav').addClass('vNav-cor2');
             }
-            else if(position <2800 && position > 2100){
-                $('.vNav ul li').removeClass('vNav-cor1');
-                $('.vNav ul li').addClass('vNav-cor2');
+            else if(position <2400 && position > 1800){
+                $('.vNav').removeClass('vNav-cor2');
+                $('.vNav').addClass('vNav-cor1');
             }
             else{
-                $('.vNav ul li').removeClass('vNav-cor2');
-                $('.vNav ul li').addClass('vNav-cor1');
+                $('.vNav').removeClass('vNav-cor1');
+                $('.vNav').addClass('vNav-cor2');
             }
         });
         
